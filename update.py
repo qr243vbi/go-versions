73fa0541b92d8b5768be9f143503995063cd69fd
qr243vbi <qr243vbi@atomicmail.io>
2026-01-18 15:45:32 +0300

    update

---
+++ update.py
@@ -0,0 +1,67 @@
+import requests
+import json
+
+# URL for the GitHub API to get releases
+url = "https://api.github.com/repos/thongtech/go-legacy-win7/releases"
+
+response = requests.get(url)
+releases_data = response.json()
+
+releases = []
+
+versions = set()
+
+for release in releases_data:
+    tag_name = release['tag_name']
+    version = tag_name.replace('v', '').split('-')[0]  # Extract version from tag
+    if (version in versions):
+        continue
+    else:
+        versions.add(version)
+    stable = True  # Assuming all fetched versions are stable
+    release_url = release['html_url']
+
+    files = []
+    for asset in release['assets']:
+        filename = asset['name']
+        #arch, platform = filename.split('-')[2:4]  # Get architecture and platform
+        download_url = f"https://github.com/thongtech/go-legacy-win7/releases/download/{tag_name}/{filename}"
+
+        extension = '.'
+
+        for i in ['.zip', '.tar.gz']:
+            if filename.endswith(i):
+                filename = filename[:-len(i)]
+                extension = i
+                break
+
+        if ('darwin' in filename) or ('race' in filename) or (extension == '.'):
+            continue
+
+        j = filename.split('.')
+        platform, arch = j[-1].split('_')
+        if platform == 'windows':
+            platform = 'win7'
+
+        files.append({
+            "filename": filename + extension,
+            "arch": arch,
+            "platform": platform,
+            "download_url": download_url
+        })
+    releases.append({
+        "version": version,
+        "stable": stable,
+        "release_url": release_url,
+        "files": files
+    })
+
+# Generate the JSON structure
+json_output = json.dumps(releases, indent=2)
+
+# Print the JSON output
+print(json_output)
+
+# Optionally, you can save it to a file
+with open("versions-manifest.json", "w") as json_file:
+    json_file.write(json_output)
